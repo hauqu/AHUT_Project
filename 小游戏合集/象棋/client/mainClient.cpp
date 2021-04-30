a64d5be2d49dbaec776a1308bc7d889f95acefc4
18098423927@163.com <18098423927@163.com>
2021-04-30 13:28:20 +0800

    设置客户端程序

--- 小游戏合集/象棋/client/mainClient.cpp
+++ 小游戏合集/象棋/client/mainClient.cpp
@@ -1,13 +1,205 @@
-#include<iostream>
+/*
+	作为游戏的服务端，处理输出输出，为方便调试绘制游戏内容
+	输入输出由自身鼠标逻辑提供，用于验证游戏逻辑
+	不包含网络服务
+*/
 #include<opencv2/highgui.hpp>
 #include<opencv2/imgcodecs.hpp>
 #include<opencv2/imgproc.hpp>
 
+#include<iostream>
+#include"Map.h"
+#include"Client.h"
 using namespace std;
 using namespace cv;
+void DrawLine(Mat img);
+void DrawChess(Mat img);
+string WinName = "chess";
+int w = 10; int h = 10;
+int k = 80;
 
-int main(int argc,char **argv)
+
+Map game;
+
+Mat chessImg;
+Mat tempImg;
+
+
+Point firstStep, secondStep;
+bool first, second;
+
+void on_MouseHandle(int event, int x, int y, int flags, void* param);
+
+int main()
 {
+	chessImg = Mat(w * k + k, h * k, CV_8UC3);
+	DrawLine(chessImg);
+	DrawChess(chessImg);
+	chessImg.copyTo(tempImg);
+	namedWindow(WinName);
+	setMouseCallback(WinName, on_MouseHandle, (void*)&tempImg);
 
+	while (true)
+	{
+
+		if (first)
+		{
+			if (second)
+			{
+				bool is = game.moveChess(firstStep.x, firstStep.y, secondStep.x, secondStep.y);
+				tempImg = Mat(w * k + k, h * k, CV_8UC3);
+				DrawLine(tempImg);
+				DrawChess(tempImg);
+				if (is)
+				{
+					cout << "成功" << endl;
+				}
+				else
+				{
+					cout << "失败" << endl;
+				}
+				second = false;
+				first = false;
+
+			}
+
+
+		}
+
+
+		imshow(WinName, tempImg);
+		if (waitKey(10) == 27)
+			break;
+	}
+
+
+	std::cin.get();
 	return 0;
+}
+void DrawLine(Mat img)
+{
+	for (int i = 1; i < w; i++)
+	{
+		line(img, Point(k * i, k), Point(k * i, (h / 2) * k), Scalar(0, 0, 0));
+		line(img, Point(k * i, (h / 2 + 1) * k), Point(k * i, h * k), Scalar(0, 0, 0));
+	}
+	line(img, Point(k * (w - 1), (h / 2) * k), Point(k * (w - 1), (h / 2 + 1) * k), Scalar(0, 0, 0));
+	line(img, Point(k, (h / 2) * k), Point(k, (h / 2 + 1) * k), Scalar(0, 0, 0));
+
+	for (int i = 1; i <= h; i++)
+	{
+		line(img, Point(k, k * i), Point((w - 1) * k, k * i), Scalar(0, 0, 0));
+	}
+
+	line(img, Point(5 * k, 2 * k), Point(6 * k, 1 * k), Scalar(0, 0, 0));
+	line(img, Point(5 * k, 2 * k), Point(4 * k, 1 * k), Scalar(0, 0, 0));
+
+	line(img, Point(5 * k, 2 * k), Point(6 * k, k * 3), Scalar(0, 0, 0));
+	line(img, Point(5 * k, 2 * k), Point(4 * k, k * 3), Scalar(0, 0, 0));
+
+	line(img, Point(5 * k, 9 * k), Point(6 * k, 10 * k), Scalar(0, 0, 0));
+	line(img, Point(5 * k, 9 * k), Point(4 * k, 10 * k), Scalar(0, 0, 0));
+
+	line(img, Point(5 * k, 9 * k), Point(6 * k, k * 8), Scalar(0, 0, 0));
+	line(img, Point(5 * k, 9 * k), Point(4 * k, k * 8), Scalar(0, 0, 0));
+
+}
+
+void DrawChess(Mat img)
+{
+	int x, y;
+	for (int i = 1; i < 33; i++)
+	{
+		if (game.cs[i]->alive != false)
+		{
+			x = game.cs[i]->x;
+			y = game.cs[i]->y;
+			circle(img, Point(x * k, y * k), k / 2, Scalar(128, 128, 128), FILLED);
+			if (game.cs[i]->player)
+			{
+				circle(img, Point(x * k, y * k), k / 2 - 5, Scalar(0, 0, 0), FILLED);
+
+			}
+			else
+			{
+				circle(img, Point(x * k, y * k), k / 2 - 5, Scalar(0, 0, 255), FILLED);
+			}
+
+			putText(img, game.csName[i], Point(x * k - 40, y * k + 10),
+				FONT_HERSHEY_DUPLEX, 0.8, Scalar(255, 255, 255), 0.5);
+		}
+
+	}
+}
+
+
+void on_MouseHandle(int event, int x, int y, int flags, void* param)
+{
+	Mat& image = *(Mat*)param;
+	Point step;
+	switch (event)
+	{
+
+		if (x < k || y < k)
+		{
+			break;
+		}
+	case EVENT_MOUSEMOVE:
+	{
+
+		//cout << "(" << x << "," << y << ")" << endl;
+	}
+	break;
+	// 左键按下消息
+	case EVENT_LBUTTONDOWN:
+	{
+		//DrawLine(tempImg);
+		//DrawChess(tempImg);
+		//cout << "(" << x << "," << y << ")" << endl;
+
+		step = Point((x + k / 2) / k, (y + k / 2) / k);
+		cout << "(" << step.x << "," << step.y << ")-->";
+
+		rectangle(tempImg, Rect(step.x * k - k / 2, step.y * k - k / 2, k, k), Scalar(0, 0, 255));
+		imshow(WinName, tempImg);
+	}
+	break;
+	//左键抬起消息
+	case EVENT_LBUTTONUP:
+	{
+		step = Point((x + k / 2) / k, (y + k / 2) / k);
+		if (!first)
+		{
+			firstStep = step;
+			first = true;
+			if (game.findChess(step.x, step.y) == -1)
+			{
+				first = false;
+			}
+
+		}
+		else
+		{
+			secondStep = step;
+			second = true;
+		}
+
+		if (second)
+			cout << endl;
+
+	}
+	break;
+	case EVENT_RBUTTONDOWN:
+	{
+		if (second)
+		{
+			second = false;
+		}
+		else if (first)
+		{
+			first = false;
+		}
+	}
+	break;
+	}
 }
